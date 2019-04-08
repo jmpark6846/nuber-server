b2a2165ce95e1cf8fd3a755b5038ead2ac432b51
jmpark6846 <jmpark6846@gmail.com>
2019-04-08 23:33:25 +0900

    subscribe 인증하기

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -13,12 +13,15 @@ const resolvers: Resolvers = {
       async (
         _,
         args: ReportMovementMutationArgs,
-        { req }
+        { req, pubSub }
       ): Promise<ReportMovementResponse> => {
         const user: User = req.user;
         const notNull = cleanNullArgs(args);
         try {
           await User.update({ id: user.id }, { ...notNull });
+          const updatedUser = await User.findOne({ id: user.id })
+          pubSub.publish("DriverUpdate", { DriverSubscription: updatedUser });
+          
           return {
             ok: true,
             error: null
@@ -33,6 +36,5 @@ const resolvers: Resolvers = {
     )
   }
 };
-
 
 export default resolvers;
